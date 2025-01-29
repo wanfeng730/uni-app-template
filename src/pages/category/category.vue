d10fd9d9d04619bb6e58a379c66b6b28ebdb241b
wanfeng730 <2580330397@qq.com>
2025-01-29 17:21:06 +0800

    feat: init

---
+++ src/pages/category/category.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+//
+</script>
+
+<template>
+  <view class="category">category</view>
+  <image
+    src=""
+    mode="aspectFill"
+  />
+</template>
+
+<style lang="scss">
+//
+</style>
